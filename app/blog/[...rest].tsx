5b0fefb69aadf07ffb7fc77be3025603f8501eab
PlatenPrime <proshta@gmail.com>
2023-07-19 08:21:01 +0300

    19.07.2023

--- app/blog/[...rest].tsx
+++
@@ -1,8 +0,0 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Text } from 'react-native';
-
-export default function Page() {
-	const { rest } = useLocalSearchParams();
-
-	return <Text>Blog post: {rest}</Text>;
-}
---
+++ app/test1.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const test1 = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} >
+			<Text>test1</Text>
+		</View>
+	)
+}
+
+export default test1
